69e48f0bb76f39cf45227875b48713dae8860b46
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-02-04 21:54:20 -0500

    Bug: Missing Admin Security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_NAME']
+
   def show
+
+    @product_count = Product.count
+
+    @category_list = Product.group(:category_id).count
+
   end
 end
